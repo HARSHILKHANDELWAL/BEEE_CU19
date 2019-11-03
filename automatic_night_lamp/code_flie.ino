43b2c50be759c8b35f2a516f995c898341780c16
HARSHILKHANDELWAL <53622598+HARSHILKHANDELWAL@users.noreply.github.com>
2019-11-04 00:50:48 +0530

    Update code_flie.ino

--- automatic_night_lamp/code_flie.ino
+++ automatic_night_lamp/code_flie.ino
@@ -1,19 +1,24 @@
 int sensorpin=A0;
-int ledpin=13;
+int redpin=13;
 int sensorvalue=0;
 void setup()
 {
+  
   Serial.begin(9600);
-  pinMode(ledpin,OUTPUT);
+  pinMode(redpin,OUTPUT);
 }
 void loop()
 {
+  
   sensorvalue=analogRead(sensorpin);
   Serial.println(sensorvalue);
- delay(100);
+ 
+  delay(100);
+  
   if(sensorvalue>300)
-    digitalWrite(ledpin,HIGH);
+    digitalWrite(redpin,HIGH);
+  
   else
-     digitalWrite(ledpin,LOW);
+     digitalWrite(redpin,LOW);
     
 }
